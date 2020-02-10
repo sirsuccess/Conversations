00ac5004209887e0816ea922fb74921e6ab14fa5
sirsuccess <kanuamani@gmail.comgit config --global user.email kanuamani@gmail.comgit config --global user.email kanuamani@gmail.com>
2020-02-11 00:00:41 +0100

    implement twit component

---
+++ src/data/twitData.js
@@ -0,0 +1,82 @@
+export default [
+  {
+    name: "Zarela Reed",
+    title: "Used Electronic Test Equipment Not Wor…",
+    comment: "Hello guys, I have been unable to make use of",
+    date: "19hrs",
+    socialMediaImg: "/assets/img/twitter.png",
+    Attachement: ""
+  },
+  {
+    name: "Ava Gregoraci",
+    title: "Used Electronic Test Equipment Not Wor…",
+    comment: "Hello guys, I have been unable to make use of",
+    date: "23hrs",
+    socialMediaImg: "/assets/img/messenger.svg",
+    Attachement: "fa fa-paperclip"
+  },
+  {
+    name: "Iruka Akuchi",
+    title: "Used Electronic Test Equipment Not Wor…",
+    comment: "Hello guys, I have been unable to make use of",
+    date: "June 1",
+    socialMediaImg: "/assets/img/telegram.svg",
+    Attachement: "fa fa-paperclip"
+  },
+  {
+    name: "Iruka Akuchi",
+    title: "Used Electronic Test Equipment Not Wor…",
+    comment: "Hello guys, I have been unable to make use of",
+    date: "June 1",
+    socialMediaImg: "/assets/img/whats-app.png",
+    Attachement: ""
+  },
+  {
+    name: "Kari Granleese",
+    title: "Used Electronic Test Equipment Not Wor…",
+    comment: "Hello guys, I have been unable to make use of",
+    date: "June 2",
+    socialMediaImg: "/assets/img/apple.png",
+    Attachement: ""
+  },
+  {
+    name: "Zarela Reed",
+    title: "Used Electronic Test Equipment Not Wor…",
+    comment: "Hello guys, I have been unable to make use of",
+    date: "19hrs",
+    socialMediaImg: "/assets/img/twitter.png",
+    Attachement: ""
+  },
+  {
+    name: "Ava Gregoraci",
+    title: "Used Electronic Test Equipment Not Wor…",
+    comment: "Hello guys, I have been unable to make use of",
+    date: "23hrs",
+    socialMediaImg: "/assets/img/messenger.svg",
+    Attachement: "fa fa-paperclip"
+  },
+  {
+    name: "Iruka Akuchi",
+    title: "Used Electronic Test Equipment Not Wor…",
+    comment: "Hello guys, I have been unable to make use of",
+    date: "June 1",
+    socialMediaImg: "/assets/img/telegram.svg",
+    Attachement: "fa fa-paperclip"
+  },
+  {
+    name: "Iruka Akuchi",
+    title: "Used Electronic Test Equipment Not Wor…",
+    comment: "Hello guys, I have been unable to make use of",
+    date: "June 1",
+    socialMediaImg: "/assets/img/whats-app.png",
+    Attachement: ""
+  },
+  {
+    name: "Kari Granleese",
+    title: "Used Electronic Test Equipment Not Wor…",
+    comment: "Hello guys, I have been unable to make use of",
+    date: "June 2",
+    socialMediaImg: "/assets/img/apple.png",
+    Attachement: ""
+  }
+];

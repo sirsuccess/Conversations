e267591676a015e32a7de772d107bcb9618e0f99
sirsuccess <kanuamani@gmail.comgit config --global user.email kanuamani@gmail.comgit config --global user.email kanuamani@gmail.com>
2020-02-10 04:35:32 +0100

    added sidebar data

---
+++ src/data/sitebarData.js
@@ -0,0 +1,18 @@
+export default [
+  {
+    icon: "/assets/img/analytics.png",
+    route: "/"
+  },
+  {
+    icon: "/assets/img/happy.png",
+    route: "/"
+  },
+  {
+    icon: "/assets/img/schedule.png",
+    route: "/"
+  },
+  {
+    icon: "/assets/img/comment.png",
+    route: "/"
+  }
+];

650f4812bf5d9903ac33dd93759ddd9edd238516
sirsuccess <kanuamani@gmail.comgit config --global user.email kanuamani@gmail.comgit config --global user.email kanuamani@gmail.com>
2020-02-20 12:10:38 +0100

    implement add comment with enter key

--- src/component/features/comments/Comments.jsx
+++ src/component/features/comments/Comments.jsx
@@ -1,13 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./Comments.css";
 import CommentData from "../../../data/commentData";
 
 function Comments() {
+  const [comments, setComments] = useState(CommentData);
+  const [inputedText, setInputedText] = useState("");
+
+  const handleChange = e => {
+    const value = e.target.value;
+    setInputedText(value);
+  };
+
+  const handleKeyDown = event => {
+    if (event.keyCode === 13) {
+      addComment();
+    }
+  };
+  function addComment() {
+    const inputObj = {
+      img: "/assets/img/me.jpg",
+      comment: inputedText
+    };
+
+    setComments([...comments, inputObj]);
+    setInputedText("");
+  }
   return (
     <div className="Comments">
       <div className="comment-scroll">
-        {CommentData.map((item, index) => (
+        {comments.map((item, index) => (
           <div className="innerComment" key={index}>
             <img src={item.img} alt="user" />
             <div className="commentMsg">{item.comment}</div>
@@ -17,8 +39,15 @@ function Comments() {
       <div className="reply">
         <img src="/assets/img/me.jpg" alt="user" />
         <div className="replyInput">
-          <input type="text" placeholder="Write your comment…." />
-          <div className="send">
+          <input
+            type="text"
+            className="commentInput"
+            placeholder="Write your comment…."
+            value={inputedText}
+            onChange={handleChange}
+            onKeyDown={handleKeyDown}
+          />
+          <div className="send" onClick={addComment}>
             <i class="fa fa-paper-plane"></i>
           </div>
         </div>
